134466be0942a3f0126b04d64ff405ade0cd137e
Alex Gusev <alex@flancer64.com>
2016-03-31 12:36:32 +0300

    MOBI-265 - Merge Common & M2 modules and clean up extra code (Setup/Schema&Data)

--- src/Setup/InstallData.php
+++ src/Setup/InstallData.php
@@ -6,10 +6,15 @@
  */
 namespace Praxigento\BonusLoyalty\Setup;
 
-class InstallData extends \Praxigento\Core\Setup\Data\Base {
+use Magento\Framework\Setup\ModuleContextInterface;
+use Praxigento\Core\Setup\Data\SchemaSetupInterface;
 
-    public function __construct() {
-        parent::__construct('Praxigento\Bonus\Loyalty\Lib\Setup\Data');
+class InstallData extends \Praxigento\Core\Setup\Data\Base
+{
+    protected function _setup(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        // TODO: Implement _setup() method.
     }
+
 
 }
