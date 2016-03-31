b2e4a397d56fa4ebecaef7abeef6beea930fabe8
Alex Gusev <alex@flancer64.com>
2016-03-31 15:08:32 +0300

    MOBI-265 - data setup migration

--- src/Setup/InstallData.php
+++ src/Setup/InstallData.php
@@ -8,13 +8,39 @@ namespace Praxigento\BonusLoyalty\Setup;
 
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Praxigento\Accounting\Lib\Entity\Type\Operation as TypeOperation;
+use Praxigento\Bonus\Base\Lib\Entity\Type\Calc as TypeCalc;
+use Praxigento\Bonus\Loyalty\Lib\Config as Cfg;
 
 class InstallData extends \Praxigento\Core\Setup\Data\Base
 {
-    protected function _setup(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    private function _addAccountingOperationsTypes()
     {
-        // TODO: Implement _setup() method.
+        $this->_getConn()->insertArray(
+            $this->_getTableName(TypeOperation::ENTITY_NAME),
+            [TypeOperation::ATTR_CODE, TypeOperation::ATTR_NOTE],
+            [
+                [Cfg::CODE_TYPE_OPER_BONUS_LOYALTY, 'Loyalty bonus.']
+            ]
+        );
     }
 
+    private function _addBonusCalculationsTypes()
+    {
+        $this->_getConn()->insertArray(
+            $this->_getTableName(TypeCalc::ENTITY_NAME),
+            [TypeCalc::ATTR_CODE, TypeCalc::ATTR_NOTE],
+            [
+                [Cfg::CODE_TYPE_CALC_COMPRESSION, 'Compression for Loyalty bonus.'],
+                [Cfg::CODE_TYPE_CALC_QUALIFICATION, 'Qualification parameters for Loyalty bonus.'],
+                [Cfg::CODE_TYPE_CALC_BONUS, 'Loyalty bonus itself.']
+            ]
+        );
+    }
 
+    protected function _setup(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $this->_addAccountingOperationsTypes();
+        $this->_addBonusCalculationsTypes();
+    }
 }
