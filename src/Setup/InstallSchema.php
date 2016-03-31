98dcd8d60f9a0c85022cbe80eefcab622c1bf91f
Alex Gusev <alex@flancer64.com>
2016-03-31 14:07:51 +0300

    MOBI-265 - scheme setup migration

--- src/Setup/InstallSchema.php
+++ src/Setup/InstallSchema.php
@@ -7,12 +7,29 @@ namespace Praxigento\BonusLoyalty\Setup;
 
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
+use Praxigento\Bonus\Loyalty\Lib\Entity\Cfg\Param;
+use Praxigento\Bonus\Loyalty\Lib\Entity\Qualification;
 
 class InstallSchema extends \Praxigento\Core\Setup\Schema\Base
 {
     protected function _setup(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
-        // TODO: Implement _setup() method.
+        /**
+         * Read and parse JSON schema.
+         */
+        $pathToFile = __DIR__ . '/../etc/dem.json';
+        $pathToNode = '/dBEAR/package/Praxigento/package/Bonus/package/Loyalty';
+        $demPackage = $this->_toolDem->readDemPackage($pathToFile, $pathToNode);
+
+        /* Cfg/ Param */
+        $entityAlias = Param::ENTITY_NAME;
+        $demEntity = $demPackage['package']['Config']['entity']['Param'];
+        $this->_toolDem->createEntity($entityAlias, $demEntity);
+
+        /* Qualification */
+        $entityAlias = Qualification::ENTITY_NAME;
+        $demEntity = $demPackage['entity']['Qualification'];
+        $this->_toolDem->createEntity($entityAlias, $demEntity);
     }
 
 
