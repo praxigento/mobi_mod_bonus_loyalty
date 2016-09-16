e519a305985bc839eecb1ff2427711dbdfd0649b
Alex Gusev <alex@flancer64.com>
2016-09-16 13:26:54 +0300

    MOBI-447 - Refresh loyalty bonus structure

--- src/Data/Entity/Cfg/Param.php
+++ src/Data/Entity/Cfg/Param.php
@@ -4,6 +4,7 @@
  */
 namespace Praxigento\BonusLoyalty\Data\Entity\Cfg;
 
+
 class Param
     extends \Praxigento\Core\Data\Entity\Base
 {
@@ -13,8 +14,76 @@ class Param
     const ATTR_RANK_ID = 'rank_id';
     const ENTITY_NAME = 'prxgt_bon_loyal_cfg_param';
 
+    /**
+     * @return double
+     */
+    public function getGv()
+    {
+        $result = parent::getData(self::ATTR_GV);
+        return $result;
+    }
+
     public function getPrimaryKeyAttrs()
     {
         return [self::ATTR_RANK_ID];
+    }
+
+    /**
+     * @return int
+     */
+    public function getPsaa()
+    {
+        $result = parent::getData(self::ATTR_PSAA);
+        return $result;
+    }
+
+    /**
+     * @return double
+     */
+    public function getPv()
+    {
+        $result = parent::getData(self::ATTR_PV);
+        return $result;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRankId()
+    {
+        $result = parent::getData(self::ATTR_RANK_ID);
+        return $result;
+    }
+
+    /**
+     * @param double $data
+     */
+    public function setGv($data)
+    {
+        parent::setData(self::ATTR_GV, $data);
+    }
+
+    /**
+     * @param int $data
+     */
+    public function setPsaa($data)
+    {
+        parent::setData(self::ATTR_PSAA, $data);
+    }
+
+    /**
+     * @param double $data
+     */
+    public function setPv($data)
+    {
+        parent::setData(self::ATTR_PV, $data);
+    }
+
+    /**
+     * @param int $data
+     */
+    public function setRankId($data)
+    {
+        parent::setData(self::ATTR_RANK_ID, $data);
     }
 }
--- src/Data/Entity/Qualification.php
+++ src/Data/Entity/Qualification.php
@@ -13,8 +13,77 @@ class Qualification
     const ATTR_PV = 'pv';
     const ENTITY_NAME = 'prxgt_bon_loyal_qual';
 
+    /**
+     * @return double
+     */
+    public function getGv()
+    {
+        $result = parent::getData(self::ATTR_GV);
+        return $result;
+    }
+
     public function getPrimaryKeyAttrs()
     {
         return [self::ATTR_COMPRESS_ID];
     }
+
+    /**
+     * @return int
+     */
+    public function getPsaa()
+    {
+        $result = parent::getData(self::ATTR_PSAA);
+        return $result;
+    }
+
+    /**
+     * @return double
+     */
+    public function getPv()
+    {
+        $result = parent::getData(self::ATTR_PV);
+        return $result;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCompressId()
+    {
+        $result = parent::getData(self::ATTR_COMPRESS_ID);
+        return $result;
+    }
+
+    /**
+     * @param double $data
+     */
+    public function setGv($data)
+    {
+        parent::setData(self::ATTR_GV, $data);
+    }
+
+    /**
+     * @param int $data
+     */
+    public function setPsaa($data)
+    {
+        parent::setData(self::ATTR_PSAA, $data);
+    }
+
+    /**
+     * @param double $data
+     */
+    public function setPv($data)
+    {
+        parent::setData(self::ATTR_PV, $data);
+    }
+
+    /**
+     * @param int $data
+     */
+    public function setCompressId($data)
+    {
+        parent::setData(self::ATTR_COMPRESS_ID, $data);
+    }
+
 }
