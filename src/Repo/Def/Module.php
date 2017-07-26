8b4d4bc165fe26b20b2c9e279308bfc6f4bb1688
dmitriimakhov <dmitriimakhov@gmail.com>
2017-07-26 15:47:09 +0300

    MOBI-805 # Bonus Base module removing Def dir

--- src/Repo/Def/Module.php
+++ src/Repo/Def/Module.php
@@ -20,11 +20,11 @@ class Module extends Db implements IModule
     protected $_manTrans;
     /** @var \Praxigento\Core\Repo\IGeneric */
     protected $_repoBasic;
-    /** @var \Praxigento\BonusBase\Repo\Entity\Cfg\Def\Generation */
+    /** @var \Praxigento\BonusBase\Repo\Entity\Cfg\Generation */
     protected $_repoBonusCfgGen;
-    /** @var  \Praxigento\BonusBase\Repo\Entity\Log\Def\Sales */
+    /** @var  \Praxigento\BonusBase\Repo\Entity\Log\Sales */
     protected $_repoLogSales;
-    /** @var \Praxigento\BonusBase\Repo\Entity\Type\Def\Calc */
+    /** @var \Praxigento\BonusBase\Repo\Entity\Type\Calc */
     protected $_repoTypeCalc;
     /** @var  \Praxigento\Core\Tool\IPeriod */
     protected $_toolPeriod;
@@ -33,9 +33,9 @@ class Module extends Db implements IModule
         \Magento\Framework\App\ResourceConnection $resource,
         \Praxigento\Core\Transaction\Database\IManager $manTrans,
         \Praxigento\Core\Repo\IGeneric $repoBasic,
-        \Praxigento\BonusBase\Repo\Entity\Cfg\Def\Generation $repoBonusCfgGen,
-        \Praxigento\BonusBase\Repo\Entity\Log\Def\Sales $repoLogSales,
-        \Praxigento\BonusBase\Repo\Entity\Type\Def\Calc $repoTypeCalc,
+        \Praxigento\BonusBase\Repo\Entity\Cfg\Generation $repoBonusCfgGen,
+        \Praxigento\BonusBase\Repo\Entity\Log\Sales $repoLogSales,
+        \Praxigento\BonusBase\Repo\Entity\Type\Calc $repoTypeCalc,
         \Praxigento\Core\Tool\IPeriod $toolPeriod
     ) {
         parent::__construct($resource);
--- src/Service/Calc/Call.php
+++ src/Service/Calc/Call.php
@@ -30,11 +30,11 @@ class Call
     protected $_callWalletOperation;
     /** @var  \Praxigento\Core\Transaction\Database\IManager */
     protected $_manTrans;
-    /** @var  \Praxigento\BonusBase\Repo\Entity\Def\Compress */
+    /** @var  \Praxigento\BonusBase\Repo\Entity\Compress */
     protected $_repoBonusCompress;
     /** @var \Praxigento\BonusBase\Repo\Service\IModule */
     protected $_repoBonusService;
-    /** @var \Praxigento\BonusBase\Repo\Entity\Type\Def\Calc */
+    /** @var \Praxigento\BonusBase\Repo\Entity\Type\Calc */
     protected $_repoBonusTypeCalc;
     /** @var \Praxigento\BonusLoyalty\Repo\IModule */
     protected $_repoMod;
@@ -52,8 +52,8 @@ class Call
      * @param \Praxigento\Core\Transaction\Database\IManager $manTrans
      * @param \Praxigento\BonusLoyalty\Repo\IModule $repoMod
      * @param \Praxigento\BonusBase\Repo\Service\IModule $repoBonusService
-     * @param \Praxigento\BonusBase\Repo\Entity\Def\Compress $repoBonusCompress
-     * @param \Praxigento\BonusBase\Repo\Entity\Type\Def\Calc $repoBonusTypeCalc
+     * @param \Praxigento\BonusBase\Repo\Entity\Compress $repoBonusCompress
+     * @param \Praxigento\BonusBase\Repo\Entity\Type\Calc $repoBonusTypeCalc
      * @param \Praxigento\BonusBase\Service\ICompress $callBaseCompress
      * @param \Praxigento\BonusBase\Service\IPeriod $callBasePeriod
      * @param \Praxigento\Downline\Service\ISnap $callDownlineSnap
@@ -69,8 +69,8 @@ class Call
         \Praxigento\Core\Transaction\Database\IManager $manTrans,
         \Praxigento\BonusLoyalty\Repo\IModule $repoMod,
         \Praxigento\BonusBase\Repo\Service\IModule $repoBonusService,
-        \Praxigento\BonusBase\Repo\Entity\Def\Compress $repoBonusCompress,
-        \Praxigento\BonusBase\Repo\Entity\Type\Def\Calc $repoBonusTypeCalc,
+        \Praxigento\BonusBase\Repo\Entity\Compress $repoBonusCompress,
+        \Praxigento\BonusBase\Repo\Entity\Type\Calc $repoBonusTypeCalc,
         \Praxigento\BonusBase\Service\ICompress $callBaseCompress,
         \Praxigento\BonusBase\Service\IPeriod $callBasePeriod,
         \Praxigento\Downline\Service\ISnap $callDownlineSnap,
