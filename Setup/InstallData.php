cd77f2cda9e7ce1c80079f409372a40b7c022d26
Alex Gusev <alex@flancer64.com>
2018-03-22 21:52:26 +0200

    MOBI-1222 Restructure .\Repo\ namespace in modules

--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -8,7 +8,7 @@ namespace Praxigento\BonusLoyalty\Setup;
 
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
-use Praxigento\Accounting\Repo\Entity\Data\Type\Operation as TypeOperation;
+use Praxigento\Accounting\Repo\Data\Type\Operation as TypeOperation;
 use Praxigento\BonusBase\Repo\Entity\Data\Type\Calc as TypeCalc;
 use Praxigento\BonusLoyalty\Config as Cfg;
 
